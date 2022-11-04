5cd451e41dc6c1d3fd2be58317c09a95e562f79d
Sarathramakrishnan <113658370+Sarathramakrishnan@users.noreply.github.com>
2022-11-04 18:33:42 +0530

    Add files via upload

---
+++ Assignments/Sarathkumar(TL)/Assignment-2/connect.py
@@ -0,0 +1,3 @@
+import ibm_db
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b0aebb68-94fa-46ec-a1fc-1c999edb6187.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=31249;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=txz44264;PWD=0e7ZwhLpS1dJFh9f","","")
+print(conn)
