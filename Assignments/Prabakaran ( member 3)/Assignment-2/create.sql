eef241ec14f82f919df5b1cd8f1cb1cf9dce3a88
Prabakaranelaya <113659147+Prabakaranelaya@users.noreply.github.com>
2022-11-05 14:52:53 +0530

    Add files via upload

---
+++ Assignments/Prabakaran ( member 3)/Assignment-2/create.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "TXZ44264"."User Details" ( User CHAR(30) NOT NULL ,Roll INT NOT NULL ,Email CHAR(30) NOT NULL ,Password CHAR(30) NOT NULL )
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'jeeva',
+1,
+'jeevas1062@mail.com',
+'jeeva1'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'Lokesh',
+2,
+'llokesha07@mail.com',
+'Lokesh2'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'prabakaran',
+3,
+'prabakaranappu9159@mail.com',
+'prabakaran3'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'sarathkumar',
+4,
+'sarathsarath53381@mail.com',
+'sarathkumar4'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'dummy',
+5,
+'dummy@mail.com',
+'dummy5'
+);
